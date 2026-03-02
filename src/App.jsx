afd011b51fbc4cc3d73aabe14266a02bbd3d83bf
Arya Waskar <aryawaskar1@gmail.com>
2026-03-02 23:39:52 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import { BrowserRouter,Routes,Router, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import ProductDetail from './pages/ProductDetail'
-import Wishlist from './pages/WishList'
+import WishList from './pages/WishList'
 import Compare from './pages/Compare'
 import './App.css'
 
@@ -15,7 +15,7 @@ function App() {
     <Routes>
       <Route path='/' element={<Home/>}/>
       <Route path='/product/:id' element={<ProductDetail/>}/>
-      <Route path='/wishlist' element={<Wishlist/>}></Route>
+      <Route path='/wishlist' element={<WishList/>}></Route>
       <Route path="/compare" element={<Compare />} />
        </Routes>
   </BrowserRouter>
